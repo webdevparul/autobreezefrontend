fc47ab9d0812b326ccca881c7319234b6dae3dfb
singhsunny26sep <webdevelopernavin@gmail.com>
2024-10-01 19:46:12 +0545

    newchanges

--- src/components/brand.jsx
+++ src/components/brand.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Marquee from "react-fast-marquee";
@@ -10,23 +10,23 @@ const Brand = () => {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 5,
-      slidesToSlide: 3 // optional, default to 1.
+      slidesToSlide: 3, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 2 // optional, default to 1.
+      slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 6,
-      slidesToSlide: 1 // optional, default to 1.
-    }
+      slidesToSlide: 1, // optional, default to 1.
+    },
   };
-  
+
   return (
     <div className="">
-    {/* <motion.div
+      {/* <motion.div
     className="bg-theme-dark w-100 pt-2 pb-2"
     initial={{ x: '100vw' }} // Start off-screen (left side)
     animate={{ x: '-100%' }} 
@@ -37,40 +37,106 @@ const Brand = () => {
       repeatType: 'loop', // Go back and forth
     }}
     > */}
-    <div className="bg-theme-dark w-100  pt-5 pb-5">
+      <div className="bg-theme-dark w-100  pt-5 pb-5">
         <div className="px-4">
-      <div className="row parent">
-      {/* <Carousel responsive={responsive} infinite={true} autoPlay={true} co autoPlaySpeed={100} arrows={false} transitionDuration={0.5} > */}
-      <Marquee speed={80}>
+          <div className="row parent">
+            {/* <Carousel responsive={responsive} infinite={true} autoPlay={true} co autoPlaySpeed={100} arrows={false} transitionDuration={0.5} > */}
+            <Marquee speed={80}>
+              <div className="col slider">
+                <img
+                  src="./img/brand/hyundai.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              <div className="col slider">
+                <img
+                  src="./img/brand/honda.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              <div className="col slider">
+                <img
+                  src="./img/brand/landrover.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              <div className="col slider">
+                <img
+                  src="./img/brand/jeep.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              <div className="col slider">
+                <img
+                  src="./img/brand/mer.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              <div style={{ marginRight: "60px" }} className="col slider">
+                <img
+                  src="./img/brand/bmw.svg"
+                  className="img-fluid w-75 h-75 "
+                  alt=""
+                />
+              </div>
+              {/* <div className="col slider"><img src="./img/brand/hyundai.png" className="img-fluid w-75 h-75 "  alt="" /></div> */}
+              <div className="col slider">
+                <img
+                  src="./img/brand/honda.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              <div className="col slider">
+                <img
+                  src="./img/brand/landrover.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              <div className="col slider">
+                <img
+                  src="./img/brand/jeep.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              <div className="col slider">
+                <img
+                  src="./img/brand/mer.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
+              {/* <div className="col slider"><img src="./img/brand/royce.png" className="img-fluid w-75 h-75" alt="" /></div> */}
+              {/* <div className="col slider"><img src="./img/brand/suzuki.png" className="img-fluid w-75 h-75" alt="" /></div> */}
+              {/* <div className="col slider"><img src="./img/brand/purche.png" className="img-fluid w-75 h-75" alt="" /></div> */}
+              <div className="col slider">
+                <img
+                  src="./img/brand/skoda.png"
+                  className="img-fluid w-75 h-75"
+                  alt=""
+                />
+              </div>
 
-<div className="col slider"><img src="./img/brand/hyundai.png" className="img-fluid w-75 h-75"  alt="" /></div>
-        <div className="col slider"><img src="./img/brand/honda.png" className="img-fluid w-75 h-75"  alt="" /></div>
-        <div className="col slider"><img src="./img/brand/landrover.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div className="col slider"><img src="./img/brand/jeep.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div className="col slider"><img src="./img/brand/mer.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div style={{marginRight:"60px"}} className="col slider"><img src="./img/brand/bmw.svg" className="img-fluid w-75 h-75 " alt="" /></div>
-        <div className="col slider"><img src="./img/brand/hyundai.png" className="img-fluid w-75 h-75 "  alt="" /></div>
-        <div className="col slider"><img src="./img/brand/honda.png" className="img-fluid w-75 h-75"  alt="" /></div>
-        <div className="col slider"><img src="./img/brand/landrover.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div className="col slider"><img src="./img/brand/jeep.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div className="col slider"><img src="./img/brand/mer.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div className="col slider"><img src="./img/brand/royce.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div className="col slider"><img src="./img/brand/suzuki.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div className="col slider"><img src="./img/brand/purche.png" className="img-fluid w-75 h-75" alt="" /></div>
-        <div className="col slider"><img src="./img/brand/skoda.png" className="img-fluid w-75 h-75" alt="" /></div>
-
-
-        
-        
-        <div style={{marginRight:"60px"}} className="col slider"><img src="./img/brand/bmw.svg" className="img-fluid w-75 h-75 " alt="" /></div>
-
-        </Marquee>
+              <div style={{ marginRight: "60px" }} className="col slider">
+                <img
+                  src="./img/brand/bmw.svg"
+                  className="img-fluid w-75 h-75 "
+                  alt=""
+                />
+              </div>
+            </Marquee>
+          </div>
+          {/* <div className="col-3"><img src="./img/brand/honda.png" width={20} alt="" /></div> */}
+        </div>
       </div>
-        {/* <div className="col-3"><img src="./img/brand/honda.png" width={20} alt="" /></div> */}
-      </div>
-    </div>
-    {/* </motion.div> */}
-    
+      {/* </motion.div> */}
     </div>
   );
 };
