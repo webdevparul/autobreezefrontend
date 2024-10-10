e30c4f6b9faccdec5d3fb98a245003400e398388
Vishal@7831 <vishalpatel78317@gmial.com>
2024-10-10 23:47:10 +0530

    VP:add dev end point

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const LOCAL_URL=`http://localhost:7000`
 export const DEV_URL=`http://15.207.110.135:7000`
 export const PROD_URL=""
-export const URL=LOCAL_URL
+export const URL=DEV_URL
 
 export const END_POINT={
   CAR:`${URL}/car`,
